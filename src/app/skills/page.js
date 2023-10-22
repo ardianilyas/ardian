ff3e30398fd8a5e2d70085ac9ddc75e818f0f9b8
Luc1ous <ardianilyas@gmail.com>
2023-10-22 18:45:49 +0700

    update skill page

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -9,15 +9,19 @@ export default function Skills() {
       <h1 className='text-4xl font-bold text-center'>Skills</h1>
       <p className='text-center mb-2'>Here is my skills stack in Web Programming.</p>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10'>
-        {skills.map((skill, index) => {
-          return (
-            <div key={index} className='bg-white p-6 rounded-md hover:shadow-md cursor-pointer'>
-              <h1 className='font-bold text-xl'>{skill.name}</h1>
-              <p className='mt-2 text-sm'>{skill.desc}</p>
-            </div>
-          )
-        })}
+      <div className="max-w-7xl relative">
+        <div className="absolute -z-10 -top-10 -left-8 w-32 h-32 bg-sky-400 rounded-full mix-blend-multiply filter blur-xl"></div>
+        <div className="absolute -z-10 -top-10 left-20 w-32 h-32 bg-yellow-400 rounded-full mix-blend-multiply filter blur-xl"></div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10'>
+          {skills.map((skill, index) => {
+            return (
+              <div key={index} className='bg-white p-6 rounded-md hover:shadow-md cursor-pointer'>
+                <h1 className='font-bold text-xl'>{skill.name}</h1>
+                <p className='mt-2 text-sm'>{skill.desc}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
